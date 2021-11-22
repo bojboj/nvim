05164cf97fc76637b2a0267df4b8428183146dd6
Job Cabral <jcabral@engagednation.com>
2021-11-21 23:03:03 -0800

    Updated key mapping for nvim-dap

--- lua/setup/plugged/nvim-dap.lua
+++ lua/setup/plugged/nvim-dap.lua
@@ -12,20 +12,15 @@ require('dap.ext.vscode').load_launchjs()
 dap.defaults.fallback.terminal_win_cmd = '80vsplit new'
 
 local keymap = vim.api.nvim_set_keymap
-local opts = {noremap = true, silent = true}
+local opts = {noremap = true}
 
-keymap('n', '<leader>dh', ':lua require"dap".toggle_breakpoint()<CR>', opts)
-keymap('n', '<leader>dH', ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>", opts)
-keymap('n', '<c-k>', ':lua require"dap".step_out()<CR>', opts)
-keymap('n', '<c-l>', ':lua require"dap".step_into()<CR>', opts)
-keymap('n', '<c-j>', ':lua require"dap".step_over()<CR>', opts)
-keymap('n', '<c-h>', ':lua require"dap".continue()<CR>', opts)
+keymap('n', '<F4>', ':lua require"dap".terminate()<CR>', opts)
+keymap('n', '<F5>', ':lua require"dap".continue()<CR>', opts)
+keymap('n', '<F6>', ':lua require"dap".pause()<CR>', opts)
+keymap('n', '<F7>', ':lua require"dap".toggle_breakpoint()<CR>', opts)
+keymap('n', '<F8>', ':lua require"dap".step_into()<CR>', opts)
+keymap('n', '<F9>', ':lua require"dap".run_to_cursor()<CR>', opts)
 keymap('n', '<leader>dk', ':lua require"dap".up()<CR>', opts)
 keymap('n', '<leader>dj', ':lua require"dap".down()<CR>', opts)
-keymap('n', '<leader>dc', ':lua require"dap".terminate()<CR>', opts)
-keymap('n', '<leader>dr', ':lua require"dap".repl.toggle({}, "vsplit")<CR><C-w>l', opts)
-keymap('n', '<leader>de', ':lua require"dap".set_exception_breakpoints({"all"})<CR>', opts)
-keymap('n', '<leader>da', ':lua require"debugHelper".attach()<CR>', opts)
-keymap('n', '<leader>dA', ':lua require"debugHelper".attachToRemote()<CR>', opts)
-keymap('n', '<leader>di', ':lua require"dap.ui.widgets".hover()<CR>', opts)
-keymap('n', '<leader>d?', ':lua local widgets=require"dap.ui.widgets";widgets.centered_float(widgets.scopes)<CR>', opts)
+keymap('n', '<leader>P', ':lua require"dap.ui.widgets".hover()<CR>', opts)
+keymap('n', '<leader>L', ':lua local widgets=require"dap.ui.widgets";widgets.centered_float(widgets.scopes)<CR>', opts)

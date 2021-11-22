19e13bd16bd5c28e4c5e75dc24e12fdf4c7bb6ee
Job Cabral <jcabral@engagednation.com>
2021-11-21 18:38:20 -0800

    Removed fzf path.

--- lua/setup/neovim.lua
+++ lua/setup/neovim.lua
@@ -1,12 +1,9 @@
-local home = os.getenv('HOME')
-
 vim.opt.hidden = true             -- Enable buffer to be hidden.
 vim.opt.laststatus = 2            -- Always show status bar.
 vim.opt.mouse = 'a'               -- Mouse support.
 vim.opt.number = true             -- Enable number line.
 vim.opt.relativenumber = true     -- Enable relative number line.
 vim.opt.termguicolors = true      -- Enable rgb colors.
-vim.opt.rtp:append(home..'/.fzf') -- Add fzf path.
 
 vim.cmd('colo seoul256')
 

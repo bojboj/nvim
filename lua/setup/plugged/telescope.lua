29a5ffa8ab5b56fb5833fa2e0658594dbfc080d7
Job Cabral <jcabral@engagednation.com>
2021-11-25 23:19:17 -0800

    Updated layout for telescope.

--- lua/setup/plugged/telescope.lua
+++ lua/setup/plugged/telescope.lua
@@ -1,4 +1,10 @@
 local keymap = vim.api.nvim_set_keymap
 local opts = {noremap = true}
 
+require('telescope').setup{
+    defaults = {
+      layout_strategy = 'vertical'
+    }
+}
+
 keymap('n', '<Leader>t', ':Telescope ', opts)

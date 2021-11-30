dabfbaa10ba71b3dd48362492f70d1eda04cedcf
Job Cabral <jcabral@engagednation.com>
2021-11-30 15:22:42 -0800

    Updated telescope keymapping.

--- lua/setup/plugged/telescope.lua
+++ lua/setup/plugged/telescope.lua
@@ -7,4 +7,9 @@ require('telescope').setup{
 local keymap = vim.api.nvim_set_keymap
 local opts = {noremap = true}
 
-keymap('n', '<leader>s', ':Telescope ', opts)
+keymap('n', '<leader>S', ':Telescope ', opts)
+keymap('n', '<leader>sb', ':Telescope buffers<cr>', opts)
+keymap('n', '<leader>sf', ':Telescope git_files<cr>', opts)
+keymap('n', '<leader>sg', ':Telescope live_grep<cr>', opts)
+keymap('n', '<leader>sr', ':Telescope lsp_references<cr>', opts)
+keymap('n', '<leader>ss', ':Telescope git_status<cr>', opts)

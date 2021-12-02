1aa65e519b1ba8eb2e07ed01898c5d2b3384d6f0
Job Cabral <jcabral@engagednation.com>
2021-12-01 23:18:28 -0800

    Configured tabs

--- lua/setup/plugged/lualine.lua
+++ lua/setup/plugged/lualine.lua
@@ -3,5 +3,13 @@ require('lualine').setup {
     theme = 'seoul256',
     component_separators = {left = '', right = ''},
     section_separators = {left = '', right = ''},
+  },
+  tabline = {
+    lualine_b = {
+      {
+        'tabs',
+        mode = 1,
+      }
+    }
   }
 }

f4eef7fde5fdbfd89f662ed12532625062fd1c3f
Job Cabral <jcabral@engagednation.com>
2021-12-03 12:24:51 -0800

    Updated telescope keymappings.

--- lua/setup/plugged/telescope.lua
+++ lua/setup/plugged/telescope.lua
@@ -1,15 +1,26 @@
+local actions = require('telescope.actions')
+
 require('telescope').setup{
-    defaults = {
-      layout_strategy = 'vertical'
-    }
+  defaults = {
+    layout_strategy = 'vertical'
+  },
+  pickers = {
+    buffers = {
+      mappings = {
+        i = {
+          ['<c-d>'] = actions.delete_buffer
+        },
+      }
+    },
+  },
 }
 
 local keymap = vim.api.nvim_set_keymap
 local opts = {noremap = true}
 
-keymap('n', '<leader>S', ':Telescope ', opts)
-keymap('n', '<leader>sd', ':Telescope buffers<cr>', opts)
-keymap('n', '<leader>sf', ':Telescope git_files<cr>', opts)
-keymap('n', '<leader>sg', ':Telescope live_grep<cr>', opts)
-keymap('n', '<leader>sr', ':Telescope lsp_references<cr>', opts)
-keymap('n', '<leader>ss', ':Telescope git_status<cr>', opts)
+keymap('n', '<leader>F', ':Telescope ', opts)
+keymap('n', '<leader>fd', ':Telescope buffers<cr>', opts)
+keymap('n', '<leader>ff', ':Telescope git_files<cr>', opts)
+keymap('n', '<leader>fg', ':Telescope live_grep<cr>', opts)
+keymap('n', '<leader>fr', ':Telescope lsp_references<cr>', opts)
+keymap('n', '<leader>fs', ':Telescope git_status<cr>', opts)

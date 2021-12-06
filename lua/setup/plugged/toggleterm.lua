e10135ba337c1703f75c6afad3af0df99035eb68
debian <debian@debian.debian>
2021-12-05 23:20:09 -0800

    Updated toggleterm keymap.

--- lua/setup/plugged/toggleterm.lua
+++ lua/setup/plugged/toggleterm.lua
@@ -15,5 +15,5 @@ end
 local opts = {noremap = true, silent = true}
 local keymap = vim.api.nvim_set_keymap
 
-keymap('n', '<leader>te', '<cmd>lua _scratch_terminal_toggle()<CR>', opts)
+keymap('n', '<leader>er', '<cmd>lua _scratch_terminal_toggle()<CR>', opts)
 keymap('t', [[<C-\><C-\>]], [[<C-\><C-n>]], opts)

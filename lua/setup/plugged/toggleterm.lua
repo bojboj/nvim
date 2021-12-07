e0f6d33981a91c94eef1947aa333ff5f533b65d3
Job Cabral <jcabral@engagednation.com>
2021-12-07 09:44:27 -0800

    Updated toggleterm setup.

--- lua/setup/plugged/toggleterm.lua
+++ lua/setup/plugged/toggleterm.lua
@@ -4,17 +4,26 @@ require('toggleterm').setup{
 
 local Terminal  = require('toggleterm.terminal').Terminal
 
+local scratch_terminal = Terminal:new({
+  direction = 'float',
+})
+
+function _scratch_terminal_toggle()
+  scratch_terminal:toggle()
+end
+
 local lazygit_terminal = Terminal:new({
   cmd = 'lazygit',
   direction = 'float',
 })
 
-function _scratch_terminal_toggle()
+function _lazygit_terminal_toggle()
   lazygit_terminal:toggle()
 end
 
 local opts = {noremap = true, silent = true}
 local keymap = vim.api.nvim_set_keymap
 
-keymap('n', '<leader>lz', '<cmd>lua _scratch_terminal_toggle()<CR>', opts)
 keymap('t', [[<C-\><C-\>]], [[<C-\><C-n>]], opts)
+keymap('n', '<leader>er', '<cmd>lua _scratch_terminal_toggle()<CR>', opts)
+keymap('n', '<leader>lz', '<cmd>lua _lazygit_terminal_toggle()<CR>', opts)

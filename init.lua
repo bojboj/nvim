6d8cc474924c4b2b77436ab997ba1f468c70a28d
debian <debian@debian.debian>
2021-12-11 23:18:45 -0800

    Refoctored and removed init.lua

--- init.lua
+++ init.lua
@@ -52,4 +52,13 @@ plug('xdebug/vscode-php-debug', {dir = home..'/.config/nvim/tooling/dab_adapters
 
 vim.call('plug#end')
 
-require('setup')
+require('setup.neovim')
+require('setup.plugged.auto-session')
+require('setup.plugged.gitsigns')
+require('setup.plugged.indent-blankline')
+require('setup.plugged.lualine')
+require('setup.plugged.nvim-dap')
+require('setup.plugged.nvim-lspconfig')
+require('setup.plugged.nvim-tree')
+require('setup.plugged.nvim-treesitter')
+require('setup.plugged.telescope')

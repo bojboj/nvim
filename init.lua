36fc201018c03a9171e370498585d1346470c2ce
Job Cabral <mr.bojboj@icloud.com>
2022-01-08 23:11:23 -0800

    Fixed folder name.

--- init.lua
+++ init.lua
@@ -48,7 +48,7 @@ plug("hrsh7th/cmp-path")
 -- nvim-dap
 plug("mfussenegger/nvim-dap")
 plug("rcarriga/nvim-dap-ui")
-plug("xdebug/vscode-php-debug", {dir = home .. "/.config/nvim/tooling/dab_adapters/vscode-php-debug", ["do"] = "npm install && npm run build"})
+plug("xdebug/vscode-php-debug", {dir = home .. "/.config/nvim/tooling/dap_adapters/vscode-php-debug", ["do"] = "npm install && npm run build"})
 
 vim.call("plug#end")
 

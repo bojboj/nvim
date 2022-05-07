610ea8da928e4428edb34a5ddab2cf3c96ab31ed
Job Cabral <mr.bojboj@icloud.com>
2022-05-06 23:02:58 -0700

    Updated config.

--- lua/setup/aerial.lua
+++ lua/setup/aerial.lua
@@ -1,7 +1,6 @@
 require("aerial").setup({
   filter_kind = false,
   show_guides = true,
-  manage_folds = true,
   on_attach = function(bufnr)
     vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>a', '<cmd>AerialToggle!<CR>', {})
   end

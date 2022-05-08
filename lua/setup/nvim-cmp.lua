f4f74f3db7bdde2fae5a4ba2c1480a37dafe3638
Job Cabral <mr.bojboj@icloud.com>
2022-05-07 23:14:16 -0700

    Fixed configs after plugins updated.

---
+++ lua/setup/nvim-cmp.lua
@@ -0,0 +1,42 @@
+vim.opt.completeopt = "menu,menuone,noselect"
+
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local cmp = require("cmp")
+
+cmp.setup({
+  mapping = {
+    ["<C-Space>"] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Insert,
+      select = true,
+    },
+
+    ["<Tab>"] = function(fallback)
+    if not cmp.select_next_item() then
+      if vim.bo.buftype ~= "prompt" and has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end
+  end,
+
+    ["<S-Tab>"] = function(fallback)
+    if not cmp.select_prev_item() then
+      if vim.bo.buftype ~= "prompt" and has_words_before() then
+          cmp.complete()
+        else
+          fallback()
+      end
+    end
+  end,
+  },
+  sources = cmp.config.sources({
+    {name = "nvim_lsp"},
+    {name = "buffer"},
+    {name = "path"}
+  })
+})
